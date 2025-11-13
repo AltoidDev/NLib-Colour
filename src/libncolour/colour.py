791a73b72f8ba2d5f5c939e7b429fcc841d56d35
AltoidDev <100539243+AltoidDev@users.noreply.github.com>
2025-11-13 13:22:20 -0800

    modified for clarity

--- src/libncolour/colour.py
+++ src/libncolour/colour.py
@@ -22,12 +22,14 @@ def checkall(list:list,type):
     for x in list:
         if not isinstance(x,type):
             return False
+        
     return True
 
 
 def average(colours:list):
     validcolours:list = []
     total = (0,0,0)
+    
     for current in colours:
         curcol = (0,0,0)
         if current is tuple or isinstance(current,colour):
@@ -35,22 +37,26 @@ def average(colours:list):
         else:
             raise TypeError() # 1 or more item in list isn't a colour and can't be averaged.
         validcolours.append(curcol)
+        
     if checkall(validcolours,RGB):
         for x in validcolours:
             total[0] += x.r
             total[1] += x.g
             total[2] += x.b
+            
     elif checkall(validcolours,HSV):
         for x in validcolours:
             total[0] += x.h
             total[1] += x.s
             total[2] += x.v
+            
     else:
         print("\nNot all values in list are of the same type, so they'll just be averaged as raw numbers.\n")
         for x in validcolours:
             total[0] += x[0]
             total[1] += x[1]
             total[2] += x[2]
+            
     total[0] = total[0] / len(validcolours)
     total[1] = total[1] / len(validcolours)
     total[2] = total[2] / len(validcolours)
